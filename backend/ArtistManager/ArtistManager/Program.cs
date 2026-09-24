148f5f8067e7a43239cd4f9864a6c3da0f3d39ae
Michael Julius <mjulius161@gmail.com>
2026-09-24 14:47:35 +0200

    feat: update database connection string to use DefaultConnection and remove obsolete connection string from appsettings

--- backend/ArtistManager/ArtistManager/Program.cs
+++ backend/ArtistManager/ArtistManager/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ArtistManagerDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ArtistManagerDb")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
